4fd38d2df053b51e269192ceb74edb5d5476de52
William Wong <williamwdev@gmail.com>
2020-08-28 18:01:39 -0700

    implemented input feature and allows snake to have basic movement on arrow keys

--- snake.js
+++ snake.js
@@ -1,19 +1,30 @@
-const SNAKE_SPEED = 1;
-const snakeBody = [{ x: 11, y: 11 }]
+import { getInputDirection } from "./input.js";
+
+const SNAKE_SPEED = 8;
+const snakeBody = [
+  { x: 11, y: 11 },
+  { x: 11, y: 12 },
+  { x: 11, y: 13 },
+];
 
 function update() {
-  console.log('update snake')
+  const inputDirection = getInputDirection();
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] };
+  }
+  snakeBody[0].x += inputDirection.x
+  snakeBody[0].y += inputDirection.y
 }
 
 function draw(gameBoard) {
-  snakeBody.forEach(segment => {
-    const snakeElement = document.createElement('div');
-    snakeElement.style.gridRowStart = segment.y
-    snakeElement.style.gridColumnStart = segment.x
-    snakeElement.classList.add('snake');
+  snakeBody.forEach((segment) => {
+    const snakeElement = document.createElement("div");
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
+    snakeElement.classList.add("snake");
     gameBoard.appendChild(snakeElement);
-  })
+  });
   // console.log('draw snake')
 }
 
-export { SNAKE_SPEED, update, draw }
+export { SNAKE_SPEED, update, draw };
